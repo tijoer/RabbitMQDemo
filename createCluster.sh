08eee1c2335d887e63a6c8c4495930401f032a3f
tijoer <Tim@...>
2024-08-13 09:45:04 +0000

    asdf

--- createCluster.sh
+++ createCluster.sh
@@ -25,17 +25,17 @@ docker rm rabbit-3 --force 2>/dev/null || true
 # Start three empty Docker containers. Use --init as we want to install multiple services in the
 # container and Ownia is not a sole service that should run as PID1. Use --tty and --interactive
 # to keep the containers running and to be able to interact with them.
-docker run --detach --interactive --init --tty --rm --net rabbits --hostname rabbit-1 --name rabbit-1 --publish 8081:15672 #-v /workspaces/RabbitMqDemo/target/debian:/RabbitMqDemoMount debian-RabbitMqDemo-runner
-docker run --detach --interactive --init --tty --rm --net rabbits --hostname rabbit-2 --name rabbit-2 --publish 8082:15672 #-v /workspaces/RabbitMqDemo/target/debian:/RabbitMqDemoMount debian-RabbitMqDemo-runner
-docker run --detach --interactive --init --tty --rm --net rabbits --hostname rabbit-3 --name rabbit-3 --publish 8083:15672 #-v /workspaces/RabbitMqDemo/target/debian:/RabbitMqDemoMount debian-RabbitMqDemo-runner
+docker run --detach --interactive --init --tty --rm --net rabbits --hostname rabbit-1 --name rabbit-1 --publish 8081:15672 #-v /workspaces/RabbitMqDemo/sharedMount:/RabbitMqDemoMount debian-RabbitMqDemo-runner
+docker run --detach --interactive --init --tty --rm --net rabbits --hostname rabbit-2 --name rabbit-2 --publish 8082:15672 #-v /workspaces/RabbitMqDemo/sharedMount:/RabbitMqDemoMount debian-RabbitMqDemo-runner
+docker run --detach --interactive --init --tty --rm --net rabbits --hostname rabbit-3 --name rabbit-3 --publish 8083:15672 #-v /workspaces/RabbitMqDemo/sharedMount:/RabbitMqDemoMount debian-RabbitMqDemo-runner
 
-# Install ownia in the first container, using the create-cluster command and get the erlang_cookie
-# docker exec rabbit-1 apt-get install ./owniaMount/ownia_0.0.1_amd64.deb
-# docker exec rabbit-1 ownia install create-cluster
-ERLANG_COOKIE=$(docker exec rabbit-1 cat /var/lib/rabbitmq/.erlang.cookie)
+# # Install ownia in the first container, using the create-cluster command and get the erlang_cookie
+# # docker exec rabbit-1 apt-get install ./owniaMount/ownia_0.0.1_amd64.deb
+# # docker exec rabbit-1 ownia install create-cluster
+# ERLANG_COOKIE=$(docker exec rabbit-1 cat /var/lib/rabbitmq/.erlang.cookie)
 
-# Install ownia in the second and third container, using the join-cluster command and the erlang_cookie we fetched before
-docker exec rabbit-2 apt-get install ./owniaMount/ownia_0.0.1_amd64.deb
-docker exec rabbit-2 bash -c "ownia install join-cluster --cluster-key $ERLANG_COOKIE"
-docker exec rabbit-3 apt-get install ./owniaMount/ownia_0.0.1_amd64.deb
-docker exec rabbit-3 bash -c "ownia install join-cluster --cluster-key $ERLANG_COOKIE"
+# # Install ownia in the second and third container, using the join-cluster command and the erlang_cookie we fetched before
+# docker exec rabbit-2 apt-get install ./owniaMount/ownia_0.0.1_amd64.deb
+# docker exec rabbit-2 bash -c "ownia install join-cluster --cluster-key $ERLANG_COOKIE"
+# docker exec rabbit-3 apt-get install ./owniaMount/ownia_0.0.1_amd64.deb
+# docker exec rabbit-3 bash -c "ownia install join-cluster --cluster-key $ERLANG_COOKIE"
